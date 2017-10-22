cccfa322752643298e2462848bae90e48aaf8c69
Jingyu Yang <yangj9@uw.edu>
2017-10-22 01:15:44 -0700

    adding messages

--- src/c/t2.c
+++ src/c/t2.c
@@ -2,20 +2,38 @@
 
 static Window *s_window;
 static TextLayer *s_text_layer;
+static bool s_js_ready = true;
 
 static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+  text_layer_set_text(s_text_layer, "Select (up for ya, down for nah)");
 }
 
 static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "HELP MEE");
+  text_layer_set_text(s_text_layer, "good 2 kno bruh");
   text_layer_set_text(s_text_layer, "rip");
 
 }
 
 static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "I AM GUCCI");
-  text_layer_set_text(s_text_layer, "good 2 kno bruh");
+  text_layer_set_text(s_text_layer, "rip");
+  // Declare the dictionary's iterator
+DictionaryIterator *out_iter;
+
+  // Prepare the outbox buffer for this message
+  AppMessageResult result = app_message_outbox_begin(&out_iter);
+  if(result == APP_MSG_OK) {
+    dict_write_int(out_iter, MESSAGE_KEY_RequestData, &value, "YAY", true);
+
+    // Send this message
+    result = app_message_outbox_send();
+    if(result != APP_MSG_OK) {
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Error sending the outbox: %d", (int)result);
+    }
+  } else {
+    // The outbox cannot be used right now
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Error preparing the outbox: %d", (int)result);
+  }
+
 
 }
 
@@ -29,7 +47,7 @@ static void prv_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
+  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 30));
   text_layer_set_text(s_text_layer, "hi r u ok");
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
